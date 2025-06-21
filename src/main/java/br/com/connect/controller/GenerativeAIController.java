3c5df533dc3ea73447205a416603aea4d6bffde2
Raphael Mohandas <165943582+rmohandasrodrigues@users.noreply.github.com>
2025-06-20 23:52:45 -0300

    primeiro commit

---
+++ src/main/java/br/com/connect/controller/GenerativeAIController.java
@@ -0,0 +1,22 @@
+package br.com.connect.controller;
+
+import br.com.connect.service.ChatService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GenerativeAIController {
+
+    private final ChatService chatService;
+
+
+    public GenerativeAIController(ChatService chatService) {
+        this.chatService = chatService;
+    }
+
+    @GetMapping("ask-ai")
+    public String getResponse(@RequestParam String prompt) {
+        return chatService.getResponse(prompt);
+    }
+}
